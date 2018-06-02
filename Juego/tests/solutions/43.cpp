228fe60c7d5bc3b3265152c4f6b01733bde34d72
troyodk <troyodk@gmail.com>
2018-06-01 22:54:24 -0500

    soluciones finales

---
+++ Juego/tests/solutions/43.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	int N;
+
+	scanf("%d", &N);
+	if(N<=20)
+    {
+
+	vector<int> v(2*N);
+
+	for (int i = 0; i < 2*N; i++)
+		scanf("%d", &v[i]);
+
+	int same = 0, opp = 0;
+
+	for (int i = 0; i < N; i++){
+		same += abs(v[i] + v[i+N]);
+		opp += abs(v[i] - v[i+N]);
+	}
+
+	printf("%d\n", max(same, opp));
+    }
+    else
+    {
+        cout<<-1;
+    }
+}
